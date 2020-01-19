94788dc73e1628492a5c3fa2f04991cd40d49a6e
nmaypeter <37822464+nmaypeter@users.noreply.github.com>
2020-01-20 01:26:48 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,47 @@
+from Model import *
+
+if __name__ == '__main__':
+    dataset_seq = [1, 2, 3, 4]
+    cm_seq = [1, 2]
+    prod_seq = [1, 2]
+    wd_seq = [1, 2, 3]
+
+    for data_setting in dataset_seq:
+        dataset_name = 'email' * (data_setting == 1) + 'dnc_email' * (data_setting == 2) + \
+                       'email_Eu_core' * (data_setting == 3) + 'NetHEPT' * (data_setting == 4)
+        for cm in cm_seq:
+            cascade_model = 'ic' * (cm == 1) + 'wc' * (cm == 2)
+            for prod_setting in prod_seq:
+                product_name = 'item_lphc' * (prod_setting == 1) + 'item_hplc' * (prod_setting == 2)
+                for times in range(10):
+
+                    Model('mMdag_' + str(times), dataset_name, product_name, cascade_model).model_Mdag(r_flag=False)
+                    Model('mMdagr_' + str(times), dataset_name, product_name, cascade_model).model_Mdag(r_flag=True)
+                    Model('mdag1_' + str(times), dataset_name, product_name, cascade_model).model_dag(1, r_flag=False)
+                    Model('mdag1r_' + str(times), dataset_name, product_name, cascade_model).model_dag(1, r_flag=True)
+                    Model('mdag2_' + str(times), dataset_name, product_name, cascade_model).model_dag(2, r_flag=False)
+                    Model('mdag2r_' + str(times), dataset_name, product_name, cascade_model).model_dag(2, r_flag=True)
+                    Model('mng_' + str(times), dataset_name, product_name, cascade_model).model_ng(r_flag=False)
+                    Model('mngr_' + str(times), dataset_name, product_name, cascade_model).model_ng(r_flag=True)
+                    Model('mhd_' + str(times), dataset_name, product_name, cascade_model).model_hd()
+                    Model('mr_' + str(times), dataset_name, product_name, cascade_model).model_r()
+
+                    for wd in wd_seq:
+                        wallet_distribution_type = 'm50e25' * (wd == 1) + 'm99e96' * (wd == 2) + 'm66e34' * (wd == 3)
+
+                        Model('mMdagepw_' + str(times), dataset_name, product_name, cascade_model, wallet_distribution_type).model_Mdag(r_flag=False, epw_flag=True)
+                        Model('mMdagrepw_' + str(times), dataset_name, product_name, cascade_model, wallet_distribution_type).model_Mdag(r_flag=True, epw_flag=True)
+                        Model('mMdagpw_' + str(times), dataset_name, product_name, cascade_model, wallet_distribution_type).model_Mdag(r_flag=False)
+                        Model('mMdagrpw_' + str(times), dataset_name, product_name, cascade_model, wallet_distribution_type).model_Mdag(r_flag=True)
+                        Model('mdag1epw_' + str(times), dataset_name, product_name, cascade_model, wallet_distribution_type).model_dag(1, r_flag=False, epw_flag=True)
+                        Model('mdag1repw_' + str(times), dataset_name, product_name, cascade_model, wallet_distribution_type).model_dag(1, r_flag=True, epw_flag=True)
+                        Model('mdag2epw_' + str(times), dataset_name, product_name, cascade_model, wallet_distribution_type).model_dag(2, r_flag=False, epw_flag=True)
+                        Model('mdag2repw_' + str(times), dataset_name, product_name, cascade_model, wallet_distribution_type).model_dag(2, r_flag=True, epw_flag=True)
+                        Model('mdag1pw_' + str(times), dataset_name, product_name, cascade_model, wallet_distribution_type).model_dag(1, r_flag=False)
+                        Model('mdag1rpw_' + str(times), dataset_name, product_name, cascade_model, wallet_distribution_type).model_dag(1, r_flag=True)
+                        Model('mdag2pw_' + str(times), dataset_name, product_name, cascade_model, wallet_distribution_type).model_dag(2, r_flag=False)
+                        Model('mdag2rpw_' + str(times), dataset_name, product_name, cascade_model, wallet_distribution_type).model_dag(2, r_flag=True)
+                        Model('mngepw_' + str(times), dataset_name, product_name, cascade_model, wallet_distribution_type).model_ng(r_flag=False, epw_flag=True)
+                        Model('mngrepw_' + str(times), dataset_name, product_name, cascade_model, wallet_distribution_type).model_ng(r_flag=True, epw_flag=True)
+                        Model('mngpw_' + str(times), dataset_name, product_name, cascade_model, wallet_distribution_type).model_ng(r_flag=False)
+                        Model('mngrpw_' + str(times), dataset_name, product_name, cascade_model, wallet_distribution_type).model_ng(r_flag=True)
